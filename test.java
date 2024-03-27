33552ef1ae40ad42f5ed75de98d4dc94ffa6c2ae
xufengchao <165134183+xufengchao@users.noreply.github.com>
2024-03-27 15:15:15 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,104 @@
+package cn.hutool.core.util;
+
+import java.text.DecimalFormat;
+
+public class test {
+
+	public static void main(String[] args){
+
+		// 苹果 草莓 芒果 单价
+		int apple =8;int strawberry = 10;int mango = 10;
+		int appleWeight = 10, strawberryWeight = 5; int mangoWeight = 8;
+		System.out.println("-------------1题：两种水果 A购买的总价");
+		totalPriceA(apple, strawberry, appleWeight, strawberryWeight);
+		System.out.println("-------------2题：三种水果 B购买的总价");
+		totalPriceB(apple,strawberry,mango,appleWeight,strawberryWeight,mangoWeight);
+		System.out.println("-------------3题：折扣 C购买的总价");
+		totalPriceC(apple, strawberry, mango, appleWeight, strawberryWeight, mangoWeight);
+		System.out.println("-------------4题：折扣+满减 D购买的总价");
+		totalPriceD(apple, strawberry, mango, appleWeight, strawberryWeight, mangoWeight);
+	}
+
+	private static void totalPriceD(int apple, int strawberry, int mango, int appleWeight, int strawberryWeight, int mangoWeight) {
+		DecimalFormat df = new DecimalFormat("#.##");
+		// 判断 C是否购买了 苹果
+		if (appleWeight > 0){
+			// 苹果打8折
+			Double b = apple * 0.8;
+			int weight = appleWeight+strawberryWeight+mango;
+			if (weight == 0){
+				System.out.println("D 没有购买商品！");
+			}
+			Double totalPrice = b*appleWeight+strawberry*strawberryWeight+mango*mangoWeight;
+			if (totalPrice >= 100){
+				Double result = totalPrice / 100 ;
+				double totaltotal1 = Math.floor(result) * 10;
+				totalPrice = totalPrice - totaltotal1;
+				System.out.println("D 所购商品的总价是:"+df.format(totalPrice));
+			}else {
+				System.out.println("D 所购商品的总价是:"+df.format(totalPrice));
+			}
+		}else {
+			int weight = strawberryWeight+mangoWeight;
+			if (weight == 0){
+				System.out.println("D 没有购买商品！");
+			}else {
+				double totalPrice =strawberry*strawberryWeight+mango*mangoWeight;
+				System.out.println("没购买苹果的情况下未满减的价格是："+totalPrice);
+				if (totalPrice >= 100){
+					double result = totalPrice / 100 ;
+					double totaltotal1 = Math.floor(result) * 10 ;
+					totalPrice = totalPrice - totaltotal1;
+					System.out.println("D 所购商品的总价是:"+df.format(totalPrice));
+				}else {
+					System.out.println("D 所购商品的总价是:"+df.format(totalPrice));
+				}
+			}
+		}
+	}
+
+	private static void totalPriceC(int apple, int strawberry, int mango, int appleWeight, int strawberryWeight, int mangoWeight) {
+		// 判断 C是否购买了 苹果
+		if (appleWeight > 0){
+			// 苹果打8折
+			Double b = apple * 0.8;
+			int weight = appleWeight+strawberryWeight+mango;
+			if (weight == 0){
+				System.out.println("C 没有购买商品！");
+			}
+			Double totalPrice = b*appleWeight+strawberry*strawberryWeight+mango*mangoWeight;
+			DecimalFormat df = new DecimalFormat("#.##");
+			System.out.println("C 所购商品的总价是:"+df.format(totalPrice));
+		}else {
+			int weight = strawberryWeight+mangoWeight;
+			if (weight == 0){
+				System.out.println("C 没有购买商品！");
+			}else {
+				double totalPrice =strawberry*strawberryWeight+mango*mangoWeight;
+				System.out.println("C 所购商品的总价是："+totalPrice);
+			}
+		}
+
+	}
+
+	private static void totalPriceA(int apple, int strawberry, int appleWeight, int strawberryWeight) {
+		int weight = appleWeight+strawberryWeight;
+		if (weight == 0){
+			System.out.println("A 没有购买商品！");
+		}else {
+			double totalPrice =apple*appleWeight + strawberry*strawberryWeight;
+			System.out.println("A 所购商品的总价是："+totalPrice);
+		}
+
+	}
+	private static void totalPriceB(int apple, int strawberry,int mango, int appleWeight, int strawberryWeight,int mangoWeight) {
+		int weight = appleWeight+strawberryWeight+mangoWeight;
+		if (weight == 0){
+			System.out.println("B 没有购买商品！");
+		}else {
+			double totalPrice =apple*appleWeight + strawberry*strawberryWeight+mango*mangoWeight;
+			System.out.println("B 所购商品的总价是："+totalPrice);
+		}
+
+	}
+}
